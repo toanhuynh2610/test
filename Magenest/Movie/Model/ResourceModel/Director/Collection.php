75ae32547981d6807e366f2e0c65eb4e9fd222bb
toanhuynh2610 <36101583+toanhuynh2610@users.noreply.github.com>
2019-04-05 14:11:09 +0700

    Add files via upload

---
+++ Magenest/Movie/Model/ResourceModel/Director/Collection.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: toan
+ * Date: 21/03/2019
+ * Time: 13:11
+ */
+
+namespace Magenest\Movie\Model\ResourceModel\Movie;
+/**
+ * Subscription Collection
+ */
+class Collection extends
+    \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * Initialize resource collection
+     *
+     * @return void
+     */
+    public function _construct()
+    {
+        $this->_init('Magenest\Movie\Model\Director',
+            'Magenest\Movie\Model\ResourceModel\Director');
+    }
+}
